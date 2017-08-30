07cf1493eefce7463cf19f3bc9d7b226ad0b3642
Nathan Cazell <nathan.cazell@imageapi.com>
2017-08-30 16:11:06 -0400

    add unit tests

---
+++ spec/packer/command_spec.rb
@@ -0,0 +1,21 @@
+module Packer
+  module Binary
+    describe Command do
+
+      context 'Packer v1.0.4' do
+
+        before do
+          Packer::Binary.configure do |config|
+            config.version = '1.0.4'
+          end
+        end
+
+        it 'runs command with binary' do
+          expect(Packer::Binary::Command.run('--help > /dev/null')).to eq true
+        end
+
+      end
+
+    end
+  end
+end
